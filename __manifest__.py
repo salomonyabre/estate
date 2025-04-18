cf95015b2d73a4e8d541280b21bf8c2a8aaaa28f
salomonyabre <salomon.yabre@svsburkina.com>
2025-04-18 09:33:51 +0000

    bt20 modify ticket-ms-ticket-booking-wizard to redirect-to-passenger list

--- __manifest__.py
+++ __manifest__.py
@@ -3,6 +3,8 @@
  "depends":["base"],
  "data":[
      "security/ir.model.access.csv",
+    
+    'views/estate_property_type_wizard.xml',
      "views/res_users_views.xml",
      "views/estate_property_tag_view.xml",
      "views/estate_property_type_view.xml",
